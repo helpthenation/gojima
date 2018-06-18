532e7e1d839106cb809eb0b8d293501453bdb8f7
Rohit Srivastava <rohit.kumarsrivastava@envertis.com.au>
2018-06-18 14:01:25 +0530

    Add debugger

--- pceft_odoo_integration/static/src/js/pceft_integration.js
+++ pceft_odoo_integration/static/src/js/pceft_integration.js
@@ -16,9 +16,14 @@ odoo.define('pceft_odoo_integration.pceft_integration', function(require) {
             var _super = self._super;
             var journals = self.pos.journals;
             var amount = self.pos.get_order().get_due();
-            var is_creditcard_journal = _.find(journals, function(jnl){console.log(jnl);return jnl.id==id && jnl.is_creditcard_journal === true;});
+            var is_creditcard_journal = _.find(journals, function(jnl){console.log(jnl);
+                                                                       console.log(jnl.id);
+                                                                       console.log(jnl.is_creditcard_journal);
+                                                                       return jnl.id==id && jnl.is_creditcard_journal === true;});
+            
 
             if(is_creditcard_journal){
+                console.log("-----------------",is_creditcard_journal);
                 $.ajax('http://127.0.0.1:5000?'+amount+'&purchase').then(function(result) {
                     var result = JSON.parse(result)
                     console.log('result ', result, result['Response']);
