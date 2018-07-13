1b6cddf4140713adf07b3b98c6735894b857b8f5
rohit-envertis <rohit.kumarsrivastava@envertis.com.au>
2018-07-13 14:54:15 +0530

    general query

--- pceft_odoo_integration/static/src/js/pceft_integration.js
+++ pceft_odoo_integration/static/src/js/pceft_integration.js
@@ -22,8 +22,7 @@ odoo.define('pceft_odoo_integration.pceft_integration', function(require) {
             
 
             if(is_creditcard_journal){
-                
-                $.ajax('https://cors-anywhere.herokuapp.com/http://127.0.0.1:5000?'+amount+'&purchase').then(function(result) {
+                $.ajax({url:'http://127.0.0.1:5000?'+amount+'&purchase',crossDomain: true}).then(function(result) {
                     var result = JSON.parse(result)
                     if(result['Response']['ResponseStatus'] != "FALSE"){
                         arguments[0] = id;
